325b65e4f6b6717bd85c0b38337f950342c155d2
araaiko <araaiko.122@gmail.com>
2022-10-01 15:01:43 +0900

    husky test 3

--- src/world.ts
+++ src/world.ts
@@ -5,9 +5,12 @@ export default class World {
     this.message = message;
   }
 
-  public sayHello(elem: HTMLElement | null): void {
+  public sayHello(elem: HTMLElement | null):void {
     if (elem) {
       elem.innerText = this.message;
     }
   }
+
+
+
 }
